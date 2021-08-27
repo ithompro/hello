20c73263cb82b76c795626b37fd978dbeadae5ad
ithompro <ithompro@gmail.com>
2021-08-27 01:03:56 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,21 @@
+/**
+ * Author: Ian Pro
+ * Date:   2021/08/19
+ *
+ * A simple hello world program.
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Ian Pro!\n");
+    printf("computer science!\n");
+    printf("New changes!\n");
+    printf("New changes!\n");
+    printf("New changes!\n");
+    printf("New changes!\n");
+    printf("New changes!\n");
+    printf("New changes!\n");
+    return 0;
+}
